e12ef1bced1ba8c992b315e1331e4507b6fd2c55
herc <herculeshorse@tuta.io>
2024-01-17 21:48:44 +0200

    change from canvas api to web gl

--- resources/js/gamemanger.js
+++ resources/js/gamemanger.js
@@ -2,15 +2,17 @@
 import WebGlUtils from './webglutils.js';
 import Ball from './Ball.js';
 import Paddle from './Paddle.js';
+import Counter from './Counter.js';
 
 class GameManager{
     constructor(canvas)
     {
-        this.ctx = canvas.getContext('2d');
-        this.utils = new WebGlUtils(this.ctx);
-
+        this.ctx = canvas.getContext('webgl');
         this.width = canvas.width;
         this.height = canvas.height;
+
+        this.utils = new WebGlUtils(this.ctx, this.width, this.height);
+
 
 
         this.state = {
@@ -75,25 +77,42 @@ class GameManager{
 
     drawCompPaddle()
     {
-        this.utils.drawRectangle('#E23E57', this.computer.x, this.computer.y, this.computer.width, this.computer.height);
+        this.utils.drawRectangle('#363333', this.computer.x, this.computer.y, this.computer.width, this.computer.height);
 
     }
 
     drawScore()
     {
 
+      let playerCounter = new Counter(this.utils, {
+        "x" : this.width - 100,
+        "y" :this.height -200
+      });
 
-      this.utils.drawText(`Player     ${String(this.score.player).padStart(4, '0')}`, 55, 50);
-      this.utils.drawText(`Computer   ${String(this.score.computer).padStart(4, '0')}`, 55, 65);
+      let computerCounter = new Counter(this.utils,{
+          "x" : 50,
+          "y" :100
+      });
+
+      playerCounter.setCounter(this.score.player);
+      playerCounter.update();
+      playerCounter.draw();
+
+      computerCounter.setCounter(this.score.computer);
+      computerCounter.update();
+      computerCounter.draw();
+
+
+
     }
 
     drawPaddle(){
-        this.utils.drawRectangle('#FD7014', this.player.x, this.player.y, this.player.width, this.player.height);
+        this.utils.drawRectangle('#363333', this.player.x, this.player.y, this.player.width, this.player.height);
     }
 
     drawBall()
     {
-        this.utils.drawRectangle('#EEEEEE', this.ball.x, this.ball.y, this.ball.width, this.ball.height);
+        this.utils.drawCircle('#ffffff', this.ball.x, this.ball.y, this.ballSize,30);
     }
 
     updateComputerPaddle() {
@@ -115,7 +134,7 @@ class GameManager{
 
     increaseDifficulty()
     {
-      let factor = 9.5;
+      let factor = 5;
       this.player.width -= factor;
       this.computer.width += factor;
       this.ball.speed += 0.005;
@@ -131,41 +150,17 @@ class GameManager{
 
     draw()
     {
+        //clear the screen
         this.utils.clear(this.width, this.height);
-        this.drawPaddle();
-        this.drawBall();
-        this.drawCompPaddle();
 
         this.player.updatePosition(this.width);
+
+        this.drawPaddle();
+        this.drawCompPaddle();
         this.updateComputerPaddle();
         this.drawScore();
-    }
 
-    drawMenuScreen(){
-      this.utils.clear(this.width, this.height);
-      this.utils.drawText("PING-PONG GAME", (this.width / 2), (this.height / 2) - 50,'#FD7014', 48);
-      this.utils.drawText("Graphics project with WebGl", (this.width / 2), (this.height / 2) + 8,'#fff', 15,"normal");
-      this.utils.drawText("by Bostinaru Nicolae-Catalin", (this.width / 2), (this.height / 2) + 32,'#fff', 10,"normal");
-
-
-      //CONTROLS  
-
-      this.utils.drawText("CONTROLS", 86, (this.height - 155),'#fff', 20);
-      this.utils.drawText("LEFT ARROW KEY       MOVE TO THE LEFT", 150, (this.height - 125),'#fff', 10);
-      this.utils.drawText("RIGHT ARROW KEY      MOVE TO THE RIGHT", 152, (this.height - 105),'#fff', 10);
-      this.utils.drawText("ENTER KEY            START/STOP THE GAME", 157, (this.height - 85),'#fff', 10);
-      this.utils.drawText("'R' KEY              RESET THE GAME", 141, (this.height - 65),'#fff', 10);
-
-
-    }
-
-    drawGameOver()
-    {
-      this.utils.clear(this.width, this.height);
-      this.utils.drawText("GAME OVER", (this.width / 2), (this.height / 2) - 50,'#FD7014', 48);
-      this.utils.drawText(`Player     ${String(this.score.player).padStart(4, '0')}`,  (this.width / 2), (this.height / 2) + 10);
-      this.utils.drawText(`Computer   ${String(this.score.computer).padStart(4, '0')}`,  (this.width / 2), (this.height / 2) + 25);
-      this.utils.drawText("PRESS 'R' TO RESET THE GAME",(this.width / 2), (this.height / 2) + 45);
+        this.drawBall();
 
     }
 
@@ -205,7 +200,7 @@ class GameManager{
         let compPaddleWidth = this.computer.width;
 
         this.ball = new Ball(
-          (this.width - this.ballSize) / 2,
+          (this.width - this.ballSize + (Math.ceil(Math.random(10, 100)))) / 2,
           (this.height - this.ballSize) / 2,
           this.ballSize,
           5,
@@ -288,23 +283,18 @@ class GameManager{
     }
 
     start(){
+      
+      this.draw();
         const loop = () => {
             if(this.state.mode == "running"){
               this.update();
               this.draw();  
-            }
-            else if(this.state.mode == "gameover"){
-              this.drawGameOver();
-            }
-            else{
-              this.drawMenuScreen();
             }
             requestAnimationFrame(loop);
         };
         
         loop();
         this.handleInput();
-        this.drawMenuScreen();
       } 
 
 
--- resources/js/webglutils.js
+++ resources/js/webglutils.js
@@ -1,33 +1,146 @@
-class WebGlUtils{
+class WebGlUtils {
+    constructor(gl, width, height) {
+        this.gl = gl;
+        this.width = width;
+        this.height = height;
+        this.initShaders();
+    }
 
-    constructor(ctx){
-        this.ctx = ctx;
+    initShaders() {
+        const vertexShaderSource = `
+            attribute vec2 a_position;
+            void main() {
+                gl_Position = vec4(a_position, 0.0, 1.0);
+            }
+        `;
+
+        const fragmentShaderSource = `
+            precision mediump float;
+            uniform vec4 u_color;
+            void main() {
+                gl_FragColor = u_color;
+            }
+        `;
+
+        const vertexShader = this.compileShader(this.gl.VERTEX_SHADER, vertexShaderSource);
+        const fragmentShader = this.compileShader(this.gl.FRAGMENT_SHADER, fragmentShaderSource);
+
+        this.program = this.gl.createProgram();
+        this.gl.attachShader(this.program, vertexShader);
+        this.gl.attachShader(this.program, fragmentShader);
+        this.gl.linkProgram(this.program);
+
+        if (!this.gl.getProgramParameter(this.program, this.gl.LINK_STATUS)) {
+            console.error("Unable to initialize the shader program: " + this.gl.getProgramInfoLog(this.program));
+        }
+
+        this.gl.useProgram(this.program);
+
+        this.positionAttrib = this.gl.getAttribLocation(this.program, "a_position");
+        this.colorUniform = this.gl.getUniformLocation(this.program, "u_color");
+    }
+
+    compileShader(type, source) {
+        const shader = this.gl.createShader(type);
+        this.gl.shaderSource(shader, source);
+        this.gl.compileShader(shader);
+
+        if (!this.gl.getShaderParameter(shader, this.gl.COMPILE_STATUS)) {
+            console.error("Shader compilation failed: " + this.gl.getShaderInfoLog(shader));
+            this.gl.deleteShader(shader);
+            return null;
+        }
+
+        return shader;
     }
 
 
-    clear(width, height)
-    {
-        this.drawRectangle('#393E46', 0,0, width, height);
+    hexToColorArray(hexColor, opacity = 1.0) {
+        // Remove the '#' character if present
+        hexColor = hexColor.replace(/^#/, '');
 
+        // Parse hex values for red, green, and blue
+        const bigint = parseInt(hexColor, 16);
+        const r = (bigint >> 16) & 255;
+        const g = (bigint >> 8) & 255;
+        const b = bigint & 255;
+
+        // Normalize values to the range [0, 1]
+        const normalizedColor = [r / 255, g / 255, b / 255, opacity];
+
+        return normalizedColor;
     }
 
-    drawRectangle(color, x, y, width, height) {
+    drawRectangle(color, x, y, width, height, colorOpacity = 1.0) {
+        // Convert coordinates to normalized range [-1, 1]
+        const normalizedX = (2.0 * x) / this.width - 1.0;
+        const normalizedY = 1.0 - (2.0 * y) / this.height;
+        const normalizedWidth = (2.0 * width) / this.width;
+        const normalizedHeight = -(2.0 * height) / this.height;
+        color = this.hexToColorArray(color, colorOpacity);
 
-        this.ctx.fillStyle = color
-        this.ctx.fillRect(x, y, width, height)
+        const vertices = new Float32Array([
+            normalizedX, normalizedY,
+            normalizedX + normalizedWidth, normalizedY,
+            normalizedX, normalizedY + normalizedHeight,
+            normalizedX, normalizedY + normalizedHeight,
+            normalizedX + normalizedWidth, normalizedY,
+            normalizedX + normalizedWidth, normalizedY + normalizedHeight,
+        ]);
 
+        this.drawGeometry(color, vertices);
     }
-    drawText(text, x,y,color = '#fff', fontSize = 11, fontWeight = 'bold', fontFamily = 'courier')
-    {
-        this.ctx.font = `${fontWeight} ${fontSize}px ${fontFamily}`;
-        this.ctx.textAlign = 'center';
-        this.ctx.textBaseline = 'top';
-        this.ctx.fillStyle = color;
-        this.ctx.fillText(text, x, y);
+
+    drawCircle(color, x, y, radius, segments = 30) {
+        // Convert coordinates to normalized range [-1, 1]
+        const normalizedX = (2.0 * x) / this.width - 1.0;
+        const normalizedY = 1.0 - (2.0 * y) / this.height;
+        color = this.hexToColorArray(color);
+
+        const angleIncrement = (2 * Math.PI) / segments;
+
+        const vertices = [normalizedX, normalizedY];
+        for (let i = 0; i <= segments; i++) {
+            const angle = i * angleIncrement;
+            const xPos = normalizedX + (radius / this.width) * Math.cos(angle);
+            const yPos = normalizedY + (radius / this.height) * Math.sin(angle);
+            vertices.push(xPos, yPos);
+        }
+
+        this.drawGeometryCircle(color,color, new Float32Array(vertices));
     }
+
+    drawGeometry(color, vertices) {
+
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.gl.createBuffer());
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, vertices, this.gl.STATIC_DRAW);
+        this.gl.enableVertexAttribArray(this.positionAttrib);
+        this.gl.vertexAttribPointer(this.positionAttrib, 2, this.gl.FLOAT, false, 0, 0);
+        this.gl.uniform4fv(this.colorUniform, color);
+        this.gl.drawArrays(this.gl.TRIANGLES, 0, vertices.length / 2);
+    }
+    drawGeometryCircle(outerColor, innerColor, vertices) {
     
+        this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.gl.createBuffer());
+        this.gl.bufferData(this.gl.ARRAY_BUFFER, vertices, this.gl.STATIC_DRAW);
+    
+        this.gl.enableVertexAttribArray(this.positionAttrib);
+        this.gl.vertexAttribPointer(this.positionAttrib, 2, this.gl.FLOAT, false, 0, 0);
+    
+        // Draw the outer part of the circle with outerColor
+        this.gl.uniform4fv(this.colorUniform, outerColor);
+        this.gl.drawArrays(this.gl.TRIANGLE_FAN, 0, vertices.length / 2);
+    
+        // Draw the inner part of the circle with innerColor
+        this.gl.uniform4fv(this.colorUniform, innerColor);
+        this.gl.drawArrays(this.gl.TRIANGLE_FAN, 1, vertices.length / 2 - 1);
+    }
+    clear(){
+        let color = this.hexToColorArray('191919');
+        this.gl.clearColor(color[0], color[1], color[2], color[3]);
+        this.gl.clear(this.gl.COLOR_BUFFER_BIT);
 
-
+    }
 }
 
 export default WebGlUtils;
